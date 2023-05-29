b00cca03313efce3e567d31a7b04450b123b2ca4
Ece <ecekalpelli@gmail.com>
2023-05-29 17:02:50 +0300

    init repo + init axios

--- dist/ip.e494d247.js
+++ dist/ip.e494d247.js
@@ -13635,7 +13635,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52861" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55948" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
